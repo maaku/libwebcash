01989f40889a3290dccee7f5043ae8d3294f94f5
Mark Friedenbach <mark@friedenbach.org>
2023-04-30 10:55:53 -0700

    Add unit testing using the googletest framework.

---
+++ test/webcash.cc
@@ -0,0 +1,26 @@
+/* Copyright (c) 2022-2023 Mark Friedenbach
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <gtest/gtest.h>
+
+#include <webcash.h>
+
+TEST(gtest, assert_eq)
+{
+        ASSERT_EQ(0, 0);
+        ASSERT_NE(0, 1);
+}
+
+int main(int argc, char **argv)
+{
+        ::testing::InitGoogleTest(&argc, argv);
+
+        return RUN_ALL_TESTS();
+}
+
+/* End of File
+ */
